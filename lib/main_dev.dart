bbdb52511efa263062a9de96989e83b6a0945bc8
Berkay Uludoğan <berkay.uludogan@argedor.com>
2025-01-07 13:22:36 +0300

    firebase added

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -8,7 +8,7 @@ import 'package:leaderboard_repository/leaderboard_repository.dart';
 import 'package:super_dash/app/app.dart';
 import 'package:super_dash/audio/audio.dart';
 import 'package:super_dash/bootstrap.dart';
-import 'package:super_dash/firebase_options_dev.dart';
+import 'package:super_dash/firebase_options.dart'; 
 import 'package:super_dash/settings/persistence/persistence.dart';
 import 'package:super_dash/settings/settings.dart';
 import 'package:super_dash/share/share.dart';
